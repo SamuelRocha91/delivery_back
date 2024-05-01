bf843406f41f14286a0c5a3a72b44f110870252d
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-01 19:58:14 -0300

    feat: cria rota para registro via api

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "listing" => "products#listing"
   root to: "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post "new" => "registrations#create", as: :create_registration
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
