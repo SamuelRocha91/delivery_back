218ce0893b55268fc2a5f223701261a020b728ce
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-01 06:32:33 -0300

    feat: gera views de store com saccaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :stores
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
