dc4b4e60b64747726805812635aec502ed8a39e3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-01 11:10:09 -0300

    feat: cria rota de welcome

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   get "listing" => "products#listing"
+  root to: "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
