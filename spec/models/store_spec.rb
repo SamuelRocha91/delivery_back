abbbe0e09f209ec141524021104e9dc24887f8b3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-01 06:24:37 -0300

    feat: cria testes com shulda matchers

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -6,5 +6,16 @@ RSpec.describe Store, type: :model do
       store = Store.new name: "Greatest store ever!"
       expect(store.valid?).to eq true
     end
+
+    it "should validate presence of name" do
+      store = Store.new
+      expect(store).to_not be_valid
+    end
+
+    it "should validate presence of name" do
+      should validate_presence_of :name
+    end
+
+    it { should validate_length_of(:name).is_at_least(3) }
   end
 end
