a020242197e55b477113d79c819a86aa164dc665
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-01 11:40:00 -0300

    feat: gera migração de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :stores
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_111350) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_143843) do
   create_table "products", force: :cascade do |t|
     t.integer "store_id", null: false
     t.string "title"
@@ -24,6 +24,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_111350) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "products", "stores"
