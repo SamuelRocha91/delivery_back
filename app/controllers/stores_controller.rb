72036bb1848e6e7459c9878eb0228ce79cda0d7a
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-01 11:41:45 -0300

    feat: adiciona autenticação do usuário para a controller de store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,5 @@
 class StoresController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_store, only: %i[ show edit update destroy ]
 
   # GET /stores or /stores.json
