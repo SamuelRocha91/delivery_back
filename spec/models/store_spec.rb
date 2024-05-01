aaa30c2984872a40ccc5e3141530691ed8d9848f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-30 22:06:00 -0300

    feat: cria teste para verificar criação efetiva da loja

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should be valid when name is filled" do
+      store = Store.new name: "Greatest store ever!"
+      expect(store.valid?).to eq true
+    end
+  end
 end
