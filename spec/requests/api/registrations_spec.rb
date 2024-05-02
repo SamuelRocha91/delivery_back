356f72d49a6b962853d78480cf92e03cb6887abc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-02 14:26:07 -0300

    feat: adiciona teste api request para verificar se user admin consegue criar loja em sua role

--- spec/requests/api/registrations_spec.rb
+++ spec/requests/api/registrations_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe "registrations", type: :request do
   describe "POST /new" do
+
     it "creates a buyer user" do
       post(
         create_registration_url,
@@ -16,6 +17,24 @@ RSpec.describe "registrations", type: :request do
       user = User.find_by(email: "admin_user@example.com")
       expect(response).to be_successful
       expect(user).to be_buyer
-     end
-   end
+    end
+
+    it "fails when trying to create admin users" do
+      post(
+        create_registration_url,
+        headers: {"Accept" => "application/json"},
+        params: {
+          user: {
+            email: "admin_user@example.com",
+            password: "123456",
+            password_confirmation: "123456",
+            role: :admin
+          }
+        }
+      )
+      expect(response).to be_unprocessable
+    end 
+
+  end
+
 end
