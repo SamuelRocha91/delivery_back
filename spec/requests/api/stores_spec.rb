b5aa067bf7a8a41538f9948380ebf7162e9f1556
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-03 09:27:00 -0300

    feat: insere método para teste de requests no rails helper com login e geração de token

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe "/stores", type: :request do
   before {
     sign_in(user)
   }
+  
+  let(:credential) { Credential.create_access(:seller) }
+
+  let(:signed_in) { api_sign_in(user, credential) }
+
   describe "GET /show" do
     it "renders a successful response with stores data" do
       store = Store.create! name: "New Store", user: user
-      get "/stores/#{store.id}", headers: {"Accept" => "application/json"}
+      get "/stores/#{store.id}", headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
       json = JSON.parse(response.body)
 
       expect(json["name"]).to eq "New Store"
