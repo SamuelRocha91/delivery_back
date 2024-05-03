725d5caa8ede219ef2364113eb4763700c3e69ab
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-03 14:00:04 -0300

    feat: inserve validação de usuário na model de pedidos assegurando sua role de buyer

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   belongs_to :store
+  validate :buyer_role
+
+  private
+
+  def buyer_role
+    if !buyer.buyer?
+      errors.add(:buyer, "should be a `user.buyer`")
+    end
+  end
 end
