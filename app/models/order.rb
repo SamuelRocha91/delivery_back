8fdadaeb9c19e2a2b04add9262562d855a0511fc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-03 14:11:40 -0300

    feat: cria migração de order items e relacionamentos

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   belongs_to :store
+  has_many :order_items
+  has_many :products, through: :order_items
   validate :buyer_role
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_03_165440) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_03_171054) do
   create_table "credentials", force: :cascade do |t|
     t.integer "access"
     t.string "key", null: false
@@ -18,11 +18,23 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_03_165440) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.integer "amount", default: 1, null: false
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "buyer_id", null: false
     t.integer "store_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "state"
     t.index ["buyer_id"], name: "index_orders_on_buyer_id"
     t.index ["store_id"], name: "index_orders_on_store_id"
   end
@@ -57,6 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_03_165440) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "stores"
   add_foreign_key "orders", "users", column: "buyer_id"
   add_foreign_key "products", "stores"
