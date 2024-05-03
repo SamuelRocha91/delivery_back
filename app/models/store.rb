d98bdb00b409847d0a9a141b5d72c645093ac803
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-03 13:30:55 -0300

    feat: implementa teste de modelo de criação de lojas por admin

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,11 @@
 class Store < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+  validates :name, presence: true, length: {minimum: 3}
+  before_validation :ensure_seller
 
-    validates :name, presence: true, length: {minimum: 3}
+  private
+
+  def ensure_seller
+    self.user = nil if self.user && !self.user.seller?
+  end
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -16,6 +16,22 @@ RSpec.describe Store, type: :model do
       should validate_presence_of :name
     end
 
+    describe "belongs_to" do
+      let(:admin) {
+        User.create!(
+          email: "admin@example.com",
+          password: "123456",
+          password_confirmation: "123456",
+          role: :admin
+        )
+      }
+      
+      it "should not belong to admin users" do
+        store = Store.create(name: "store", user: admin)
+        expect(store.errors.full_messages).to eq ["User must exist"]
+      end
+    end
+    
     it { should validate_length_of(:name).is_at_least(3) }
     it { should belong_to(:user) }
   end
