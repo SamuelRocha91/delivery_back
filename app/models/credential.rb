cd8442a1a208db83fc8b3d9625d051466229b3fc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-02 21:52:15 -0300

    feat: cria metodo de credential para lançar exceção em caso de erro

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -1,3 +1,7 @@
 class Credential < ApplicationRecord
   enum :access, [:seller, :buyer]
+
+  def self.create_access(access)
+    raise "Invalid Access" if !Credential.accesses.keys.map(&:to_sym).include?(access)
+  end
 end
