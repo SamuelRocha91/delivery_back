bcb7b50e7b3ff83aa75342f4aac4a4812174bfab
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-02 21:56:43 -0300

    feat: cria teste para verificar criação de credencial de buyer

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -3,5 +3,6 @@ class Credential < ApplicationRecord
 
   def self.create_access(access)
     raise "Invalid Access" if !Credential.accesses.keys.map(&:to_sym).include?(access)
+    create! access: access, key: SecureRandom.base64(20)
   end
 end
--- spec/models/credential_spec.rb
+++ spec/models/credential_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Credential, type: :model do
         Credential.create_access(:non_existent)
       }.to raise_error "Invalid Access"
     end
+
+    it "creates a credential for a given access role" do
+      credential = Credential.create_access(:buyer)
+
+      expect(credential).to be_buyer
+      expect(credential.key).to_not be_empty
+    end
   end
 end
