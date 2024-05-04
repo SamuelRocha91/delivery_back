90cc89b8dccfb5cadeab0535da71b3c8cf47fee0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-04 17:16:58 -0300

    feat: cria método create na controller order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,20 @@
 class OrdersController < ApplicationController 
   skip_forgery_protection
   before_action :only_buyers!, :authenticate!
+
+  def create
+    @order = order.new(order_params)
+    @order.buyer = current_user
+    if @order.save
+      render json: {order: @order}
+    else
+      remder json: {errors: @order.errors, status: unprocessable_entity}
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit([:store_id])
+  end
 end
