44e5103d8f98e5ea0c00b6fcbc0e5850228f6944
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-03 22:51:26 -0300

    feat: acrescenta lógica de resposta em json da rota listing de products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,21 @@
-class ProductsController < ApplicationController
-    
-  before_action :authenticate_user!
-  
+class ProductsController < ApplicationController  
+  before_action :authenticate!
+
   def listing
-      if !current_user.admin?
-        redirect_to root_path, notice: "No permission for you!"
+    if request.format == Mime[:json]
+      if @user && @user.admin?
+        @products = Product.all
+        render json: { message: "Success", data: @products}
+      else
+        render json: { error: "Unauthorized" }, status: :unauthorized
       end
-      @products = Product.includes(:store)
-    end
+    else
+      if !current_user.admin?
+        redirect_to root_path, notice: "No permision for you"
+      else
+        @products = Product.includes(:store)
+      end
+    end   
+  end
 end
+
