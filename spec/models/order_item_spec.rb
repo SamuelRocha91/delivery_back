5997d594c11d8e7eb718156476296e2fef34db19
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-11 20:05:36 -0300

    feat: gera migração do active storage

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
   describe "attribute presence check" do
+
     it { should have_db_column(:amount).with_options(default: 1) }
     it { should have_db_column(:price).of_type(:decimal) }
   end
@@ -30,25 +31,5 @@ RSpec.describe OrderItem, type: :model do
       order_item = OrderItem.create(order: order, product: product, amount: 2, price: 4)
       expect(order_item.errors[:product]).to include( "product should belong to `Store`: #{order.store.name}")
     end
-  end
-end
-RSpec.describe OrderItem, type: :model do
-    let(:user) {create(:user)}
-    let(:store) {Store.create(name: "Burguer King", user: user)}
-    let(:product) { Product.create(store: store, title: "Macarrão", price: 2.00)}
-    let(:buyer) { create(:user_buyer) }
-    let(:order) { Order.create(buyer: buyer, store: store) }
-
-  describe "checking field validations" do
-  
- 
-
-    it "adds an error to the order" do
-      store_two = Store.create(name: "Mac Donalds", user: user)
-      product_two =  Product.create(store: store_two, title: "Macarrão", price: 2.00)
-      order_item = OrderItem.create(order: order, product: product_two, price: 1.00, amount: 2)
-      expect(order_item.errors[:product]).to include("product should belong to `Store`: #{order.store.name}")
-    end
-
   end
 end
