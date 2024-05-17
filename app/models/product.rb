e7f6756a58395674b68bf150310802a50a260694
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-17 09:22:55 -0300

    feat: cria testes para rota de produtos

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
   belongs_to :store
   validates :title, presence: true, length: {minimum: 3}
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true, length: {minimum: 10, maximum: 100}
+  validates :category, presence: true
 end
+
+ 
