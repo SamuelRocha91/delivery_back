d6a04670b705f3a4641a32173942bcc12066f4b5
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-24 19:10:22 -0300

    refactor: retira método de not_discarded de model de product

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,12 +9,7 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true, length: {minimum: 10, maximum: 100}
   validates :category, presence: true
-  scope :not_discarded, -> { where(discarded: false) }
 
-<<<<<<< feature/implements-tests-api
-
-=======
->>>>>>> main
 end
 
  
