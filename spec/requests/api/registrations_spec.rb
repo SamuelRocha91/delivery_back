de204603ef62fcb286332991b3fb042c728a7502
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-24 19:15:47 -0300

    feat: acrescenta novos testes de rota /me, destroy de registrations api

--- spec/requests/api/registrations_spec.rb
+++ spec/requests/api/registrations_spec.rb
@@ -1,7 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "registrations", type: :request do
+  let(:user) { create(:user_buyer)}
   let(:credential) { Credential.create_access(:buyer) }
+  let(:signed_in) { api_sign_in(user, credential) }
+
+
+  describe "get /me" do
+    
+    it "returns hash with user data" do
+      get(
+        "/me",
+        headers: {
+          "Accept" => "application/json",
+           "X-API-KEY" => credential.key,
+           "Authorization" => "Bearer #{signed_in["token"]}"
+        },
+      )
+      json = JSON.parse(response.body)
+      expect(json["email"]).to eq "johnfour@example.com"
+    end
+  end
 
   describe "POST /new" do
     
@@ -62,6 +81,47 @@ RSpec.describe "registrations", type: :request do
         }
       )
       expect(response).to be_unauthorized
+    end
+  end
+
+
+  describe "post /sign_in" do
+    before do
+      User.create!(
+        email: "seller@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :seller
+      )
+   end
+    
+    it "prevents users with roles different from credentials do sign in" do
+      post(
+        "/sign_in",
+        headers: {
+          "Accept" => "application/json",
+           "X-API-KEY" => credential.key
+        },
+        params: {
+          login: {email: "seller@example.com", password: "123456"}
+        }
+      )
+      expect(response).to be_unauthorized
+    end
+  end
+
+   describe "get /me" do
+    
+    it "returns hash with user data" do
+      delete(
+        "/deactivate_user/#{user.id}",
+        headers: {
+          "Accept" => "application/json",
+           "X-API-KEY" => credential.key,
+           "Authorization" => "Bearer #{signed_in["token"]}"
+        },
+      )
+      expect(response).to be_successful
     end
   end
 
