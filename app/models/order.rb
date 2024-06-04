652c183864ffb3fa3fa138e4983b0a53461134ee
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-04 11:00:52 -0300

    feat: maquina de estado pra gerenciar fases do pedido

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,43 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
   validate :buyer_role
+  accepts_nested_attributes_for :order_items, allow_destroy: true
 
   state_machine initial: :created do
-    event :accept do
+     event :pay do
+        transition created: :payment_pending
+     end
+
+    event :confirm_payment do
+        transition payment_pending: :payment_confirmed
+    end
+
+    event :payment_failed do
+        transition payment_pending: :payment_failed
+    end
+    
+    event :confirm_payment do
       transition created: :accepted
+    end
+
+     event :cancel do
+        transition [:created, :accepted, :in_progress, :ready_for_delivery, :in_delivery, :payment_pending, :payment_failed, :payment_confirmed,] => :canceled
+      end
+
+     event :start_progress do
+       transition accepted: :in_progress
+     end
+
+    event :ready_for_delivery do
+      transition in_progress: :ready_for_delivery
+    end
+
+    event :start_delivery do
+      transition ready_for_delivery: :in_delivery
+    end
+
+    event :deliver do
+      transition in_delivery: :delivered
     end
   end
 
