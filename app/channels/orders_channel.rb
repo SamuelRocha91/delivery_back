2e61e7e8a392ddd57404ba182f3ce2d2f36c6aef
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-04 17:58:50 -0300

    feat: cria canal de comunicações de pedidos

--- app/channels/orders_channel.rb
+++ app/channels/orders_channel.rb
@@ -1,9 +1,18 @@
 class OrdersChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "orders_channel_user_#{current_user.id}"
+    order = Order.find(params[:id])
+    stream_for order
   end
 
   def unsubscribed
-    ActionCable.server.broadcast "orders_chanel", message: data['message'] 
+    stop_all_streams
+  end
+
+  def self.notify_user(user, order, message)
+    OrdersChannel.broadcast_to(
+      user,
+      order_id: order.id,
+      message: message
+    )
   end
 end
