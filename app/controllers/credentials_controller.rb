db45d072f008b23c3e9d737d9a51fcb7bd8809c6
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-07 21:41:20 -0300

    feat: cria actions de crud na credential controller

---
+++ app/controllers/credentials_controller.rb
@@ -0,0 +1,50 @@
+class CredentialsController < ApplicationController
+  before_action :set_credential, only: [:update, :destroy]
+
+  def index
+    @credentials = Credential.all
+    @new_credential = Credential.new
+  end
+
+ def create
+  @credential = Credential.new(credential_params)
+  access = credential_params[:access].to_sym
+
+  if Credential.exists?(access: @credential.access)
+    redirect_to root_path, alert: "#{@credential.access.capitalize} credential already exists."
+  else
+    if @credential = Credential.create_access(access)
+      redirect_to root_path, notice: 'Credential was successfully created.'
+    else
+      render :new, alert: 'Failed to create credential.'
+    end
+  end
+ end
+
+
+ def update
+  if @credential.update(key: SecureRandom.base64(20))
+    redirect_to root_path, notice: 'Credential was successfully updated.'
+  else
+    @credentials = Credential.all
+    @new_credential = @credential
+    render :index
+  end
+end
+
+
+  def destroy
+    @credential.destroy
+    redirect_to root_path, notice: 'Credential was successfully destroyed.'
+  end
+
+  private
+
+  def set_credential
+    @credential = Credential.find(params[:id])
+  end
+
+  def credential_params
+    params.require(:credential).permit(:access, :key)
+  end
+end
