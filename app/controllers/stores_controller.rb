dbf5c4eee97bf7e9a539ac6511b02148bbbd2721
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-10 22:16:35 -0300

    feat: refatora controller de store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,19 +1,16 @@
 class StoresController < ApplicationController
   include ActionController::Live
-
   before_action :authenticate!, except: %i[ listing ]
   before_action :set_store, only: %i[ show edit update destroy ]
   skip_forgery_protection 
   rescue_from User::InvalidToken, with: :not_authorized
   
-  # GET /stores or /stores.json
   def index
     if request.format == Mime[:json]
       if current_user.admin? || current_user.buyer?
         page = params.fetch(:page, 1)
         @stores = Store.kept.includes(avatar_attachment: :blob).order(:name)
         @stores = @stores.where('LOWER(name) LIKE ?', "%#{params[:name].downcase}%") if params[:name].present?
-
         @stores = @stores.where(category: params[:category]) if params[:category].present?
         @stores = @stores.page(page)
       else
@@ -27,28 +24,24 @@ class StoresController < ApplicationController
   def new_order
     response.headers["Content-Type"] = "text/event-stream"
     sse = SSE.new(response.stream, retry: 300, event: "waiting-orders")
-    last_orders = nil
     begin
       sse.write({ hello: "world!"}, event: "waiting-order")
       EventMachine.run do
         EventMachine::PeriodicTimer.new(3) do
-         orders = Order.where.not(state: [:canceled, :delivered, :payment_failed, :created, :payment_pending])
-        if orders != last_orders 
-          if orders.any?
-            message = { time: Time.now, orders: orders } 
-            sse.write(message, event: "new orders")
-          else
-            sse.write({ message: "no orders" }, event: "no")
-          end
-          last_orders = orders 
-        end
+          orders = Order.where.not(state: [:canceled, :delivered, :payment_failed, :created, :payment_pending])
+            if orders.any?
+              message = { time: Time.now, orders: orders } 
+              sse.write(message, event: "new orders")
+            else
+              sse.write({ message: "no orders" }, event: "no")
+            end
         end
       end
-   rescue IOError, ActionController::Live::ClientDisconnected
-     sse.close
-   ensure
-     sse.close
-   end
+    rescue IOError, ActionController::Live::ClientDisconnected
+      sse.close
+    ensure
+      sse.close
+    end
   end
 
   def listing
@@ -65,6 +58,7 @@ class StoresController < ApplicationController
       @sellers = User.kept.where(role: :seller)
     end
   end
+
   def show
   end
 
@@ -76,7 +70,6 @@ class StoresController < ApplicationController
     if !current_user.admin?
       @store.user = current_user ? current_user : @user
     end
-
 
     respond_to do |format|
       if @store.save
@@ -103,7 +96,6 @@ class StoresController < ApplicationController
 
   def destroy
     @store.discard!
-
     respond_to do |format|
       format.html { redirect_to stores_url, notice: "Store was successfully destroyed." }
       format.json { head :no_content }
@@ -122,27 +114,28 @@ class StoresController < ApplicationController
   end
 
   private
-    def set_store
-      @store = Store.find(params[:id])
 
-      if @store.nil?
-        respond_to do |format|
-          format.html { redirect_to store_url(@store), alert: "Store not found or has been discarded." }
-          format.json { render json: { error: "Store not found or has been discarded" }, status: :not_found }
-        end
+  def set_store
+    @store = Store.find(params[:id])
+    if @store.nil?
+      respond_to do |format|
+        format.html { redirect_to store_url(@store), alert: "Store not found or has been discarded." }
+        format.json { render json: { error: "Store not found or has been discarded" }, status: :not_found }
       end
     end
+  end
 
-    def store_params
-      required = params.require(:store)
-      if current_user.admin?
-        required.permit(:name, :user_id, :avatar, :description, :category, :address, :state, :city, :cep, :number_address, :neighborhood, :cnpj, :is_open, :color_theme)
-      else
-        required.permit(:name, :avatar, :description, :category, :address, :state, :city, :cep, :number_address, :neighborhood, :cnpj, :is_open,  :color_theme)
-      end
+  def store_params
+    required = params.require(:store)
+    if current_user.admin?
+      required.permit(:name, :user_id, :avatar, :description, :category, :address, :state, :city, :cep, :number_address, :neighborhood, :cnpj, :is_open, :color_theme)
+    else
+      required.permit(:name, :avatar, :description, :category, :address, :state, :city, :cep, :number_address, :neighborhood, :cnpj, :is_open,  :color_theme)
     end
+  end
 
-    def not_authorized(e)
-      render json: {message: "Nope!"}, status: 401
-    end
+  def not_authorized(e)
+    render json: {message: "Nope!"}, status: 401
+  end
+
 end
