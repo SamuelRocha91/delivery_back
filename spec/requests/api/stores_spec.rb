f3924cc3cd746ab238b45a32d57449414dcb2786
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-13 17:54:41 -0300

    feat: incrementa testes de request de stores

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "/stores", type: :request do
-  let(:user) { create(:user)}
+  let(:user) { create(:user, :seller)}
 
   let(:credential) { Credential.create_access(:seller) }
 
   let(:signed_in) { api_sign_in(user, credential) }
 
-  let(:store) { Store.create name: "New Store", user: user}
+  let(:store) { create(:store, name: "New Store", user: user)}
 
   describe "GET /show" do
     it "renders a successful response with stores data" do
@@ -17,7 +17,7 @@ RSpec.describe "/stores", type: :request do
       expect(json["name"]).to eq "New Store"
     end
 
-     it "renders a failure response when accessing an invalid store" do
+    it "renders a failure response when accessing an invalid store" do
       get "/stores/2", headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
 
       expect(response).to have_http_status(:not_found)
@@ -27,18 +27,13 @@ RSpec.describe "/stores", type: :request do
    describe "GET /index" do
     
     it "renders a successful response with stores data" do
-      store = Store.create name: "New Store", user: user
       get "/stores", headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
 
       expect(response).to have_http_status(:ok)
-
       json_response = JSON.parse(response.body)
-      puts json_response.inspect
       expect(json_response["result"]["stores"]).to be_an(Array)
       expect(json_response["result"]["stores"].length).to eq(1)
-
-      expect(json_response["result"]["stores"][0]["id"]).to eq(store.id)
-      expect(json_response["result"]["stores"][0]["name"]).to eq(store.name)
+      expect(json_response["result"]["stores"][0]["id"]).to eq(1)
     end
   end
 
@@ -52,7 +47,7 @@ RSpec.describe "/stores", type: :request do
           }
         }
       json = JSON.parse(response.body)
-      expect(json["id"]).to eq 1
+      expect(json["id"]).to eq 2
       expect(json["name"]).to eq "Lojas mercadinho"
       expect(response.status).to eq 201
      end
@@ -73,29 +68,46 @@ RSpec.describe "/stores", type: :request do
   end
 
 
-   describe "PUT /stores" do
-     it "update stores successfully" do
-        put "/stores/#{store.id}",
-        headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"},
-        params: {
-          store: {
-           name: "gmail" 
-          }
+  describe "PUT /stores" do
+    it "update stores successfully" do
+      put "/stores/#{store.id}",
+      headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"},
+      params: {
+        store: {
+          name: "gmail" 
         }
-        json = JSON.parse(response.body)
-        expect(json["name"]).to eq "gmail"
-        expect(response.status).to eq 200
-     end
-   end
+      }
+      json = JSON.parse(response.body)
+      expect(json["name"]).to eq "gmail"
+      expect(response.status).to eq 200
+    end
+
+    it "returns an error if the store does not exist" do
+      put "/stores/999",
+      headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"},
+      params: {
+        store: {
+          name: "Updated Store Name"
+        }
+      }
+      expect(response.status).to eq 404
+    end
+  end
 
 
-   describe "DELETE /stores" do
-     it "delete stores successfully" do
-        delete "/stores/#{store.id}",
-        headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
-        expect(response.status).to eq 204
-        expect(store.reload.discarded_at).not_to be_nil
-     end
-   end
+  describe "DELETE /stores" do
+    it "delete stores successfully" do
+      delete "/stores/#{store.id}",
+      headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
+      expect(response.status).to eq 204
+      expect(store.reload.discarded_at).not_to be_nil
+    end
+
+    it "returns an error if the store does not exist" do
+      delete "/stores/999",
+      headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
+      expect(response.status).to eq 404
+    end
+  end
 
 end
