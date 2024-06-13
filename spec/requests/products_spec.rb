6bb953e507c52f3d6791b9a27d12e427e6daaf94
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-13 17:21:47 -0300

    feat: faz testes de requests de products

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,28 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-  let(:user) {
-    create(:user)
-  }
+  let(:user) { create(:user, :seller)}
 
-  let(:admin) {
-    create(:user_admin)
-  }
+  let(:admin) { create(:user, :admin)}
 
-  before {
-    sign_in(admin)
-  }
+  let(:store) { create(:store, user: user)}
 
-  before {
-    Product.create(store: Store.create(name: "Store 3", user: user), title: "japan", price: 9.50, description: "hambuerguer massa que e otimo", category: "massas" )
-  }
+  before {sign_in(admin)}
+
+  before { create(:product, store: store)}
 
   describe "GET /listing" do
     it "renders a successful response" do
       get listing_url
       expect(response). to be_successful
-      expect(response.body).to include "japan"
+    end
+
+    it "displays the product details" do
+      product = create(:product, title: "Test Product", store: store)
+      get listing_url(product)
+      expect(response).to be_successful
+      expect(response.body).to include("Test Product")
     end
   end
+
+  describe "POST /products" do
+    it "creates a new product" do
+      product_params = {
+        title: "New Product",
+        price: 29.99,
+        description: "This is a new product",
+        category: "bebidas"
+      }
+      post store_products_url(store_id: store.id), params: { product: product_params }
+      expect(response).to have_http_status(:found)
+      follow_redirect!
+      expect(response).to have_http_status(:ok)
+      expect(Product.last.title).to eq("New Product")
+      expect(Product.last.price).to eq(29.99)
+      expect(Product.last.description).to eq("This is a new product")
+    end
+
+     it "does not create a new product with invalid parameters" do
+      product_params = {
+        title: "",
+        price: 29.99,
+        description: "This is a new product",
+        user_id: user.id
+      }
+
+      expect {
+        post store_products_url(store_id: store.id), params: { product: product_params }
+      }.to_not change(Product, :count)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe "PUT /products/:id" do
+    it "updates an existing product" do
+     product = create(:product, title: "Old Product", price: 9.99, description: "This is an old product", store: store)
+     product_params = {
+      title: "Updated Product",
+      price: 19.99,
+      description: "This is an updated product"
+      }
+      put store_product_url(store_id: store.id, id: product.id), params: { product: product_params }
+      expect(response).to have_http_status(:found) 
+      follow_redirect!
+      expect(response).to have_http_status(:ok) #
+      expect(product.reload.title).to eq("Updated Product")
+    end
+
+    it "does not update a product with invalid parameters" do
+      product = create(:product, title: "Old Product", price: 9.99, description: "This is an old product", store: store )
+      product_params = {
+        title: "",
+        price: 19.99,
+        description: "This is an updated product"
+      }
+       put store_product_url(store_id: store.id, id: product.id), params: { product: product_params }
+       expect(response).to have_http_status(:unprocessable_entity)
+       expect(Product.find(product.id).title).to eq("Old Product") 
+    end
+  end
+
+    describe "DELETE /products/:id" do
+      it "deletes an existing product" do
+        product = create(:product, store: store)
+        delete store_product_url(store_id: store.id, id: product.id)
+        expect(response).to have_http_status(:found) 
+        follow_redirect!
+        expect(response).to have_http_status(:ok) 
+        expect(Product.find(product.id).discarded?).to be true
+      end
+    end
   
 end
---
+++ vendor/bundle/ruby/3.2.0/specifications/sorbet-runtime-0.5.11430.gemspec
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+# stub: sorbet-runtime 0.5.11430 ruby lib
+
+Gem::Specification.new do |s|
+  s.name = "sorbet-runtime".freeze
+  s.version = "0.5.11430"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0".freeze) if s.respond_to? :required_rubygems_version=
+  s.metadata = { "source_code_uri" => "https://github.com/sorbet/sorbet" } if s.respond_to? :metadata=
+  s.require_paths = ["lib".freeze]
+  s.authors = ["Stripe".freeze]
+  s.date = "2024-06-13"
+  s.description = "Sorbet's runtime type checking component".freeze
+  s.homepage = "https://sorbet.org".freeze
+  s.licenses = ["Apache-2.0".freeze]
+  s.required_ruby_version = Gem::Requirement.new(">= 2.7.0".freeze)
+  s.rubygems_version = "3.4.10".freeze
+  s.summary = "Sorbet runtime".freeze
+
+  s.installed_by_version = "3.4.10" if s.respond_to? :installed_by_version
+
+  s.specification_version = 4
+
+  s.add_development_dependency(%q<minitest>.freeze, ["~> 5.11"])
+  s.add_development_dependency(%q<mocha>.freeze, ["~> 2.1"])
+  s.add_development_dependency(%q<rake>.freeze, [">= 0"])
+  s.add_development_dependency(%q<rubocop>.freeze, ["= 1.57.1"])
+  s.add_development_dependency(%q<rubocop-performance>.freeze, ["= 1.13.2"])
+  s.add_development_dependency(%q<concurrent-ruby>.freeze, ["~> 1.1.5"])
+  s.add_development_dependency(%q<pry>.freeze, [">= 0"])
+  s.add_development_dependency(%q<pry-byebug>.freeze, [">= 0"])
+  s.add_development_dependency(%q<subprocess>.freeze, ["~> 1.5.3"])
+end
