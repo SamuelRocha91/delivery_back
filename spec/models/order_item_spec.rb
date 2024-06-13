42bfe092bf3513bc3ac7b114ee4223c7d5b36d80
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-13 00:21:14 -0300

    feat: readequa testes de order items

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe OrderItem, type: :model do
    
   describe "checking field validations" do
     it "creates a new order_item successfully" do
-      user = create(:user_buyer)
-      store = create(:store)
+      user = create(:user, :buyer)
+      seller = create(:user, :seller)
+      store = create(:store, user: seller)
       product = Product.create(store: store, title: "Mc DOnalds", price: 1.00, description: "hamburguer excelente do mc", category: "massas")
       order = Order.create(buyer: user, store: store)
       order_item = product.order_items.create(order: order, price: 1.00, amount: 2 )
@@ -19,29 +20,15 @@ RSpec.describe OrderItem, type: :model do
     end
 
     it "adds an error to the order" do
-      user = create(:user_buyer)
-      store = create(:store)
-      store_two = Store.create(name: "Japan", user: create(:user_two))
+      user = create(:user, :buyer)
+      seller = create(:user, :seller)
+      store = create(:store, user: seller)
+      store_two = Store.create(name: "Japan", user: create(:user, :buyer))
       product = Product.create(store: store_two, title: "Mc DOnalds", price: 1.50)
       order = Order.create(buyer: user, store: store)
       order_item = OrderItem.create(order: order, product: product, amount: 2, price: 4)
       expect(order_item.errors[:product]).to include( "product should belong to `Store`: #{order.store.name}")
     end
-  end
-
-   it "checks the associations" do
-    should belong_to(:order)
-    should belong_to(:product)
-  end
-
-  describe "checking the numericality of the fields" do
-    it { should validate_numericality_of(:amount).is_greater_than(0) }
-    it { should validate_numericality_of(:price).is_greater_than(0) }
-  end
-  
-  describe "checking the presence of the fields" do
-    it { should validate_presence_of(:amount) }
-    it { should validate_presence_of(:price) }
   end
 
 end
