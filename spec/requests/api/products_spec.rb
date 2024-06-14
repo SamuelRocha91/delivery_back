2a004e13b059262ef16d8bd87e9eef483015691e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-13 23:18:37 -0300

    feat: refatora testes de api de products

--- spec/requests/api/products_spec.rb
+++ spec/requests/api/products_spec.rb
@@ -1,28 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "/stores/:store_id/products", type: :request do
-   let(:user) {
-    create(:user)
-   }
+   let(:user) { create(:user, :seller)}
 
    let(:credential) { Credential.create_access(:seller) }
 
    let(:signed_in) { api_sign_in(user, credential) }
 
-   let(:store) {
-    Store.create(name: "Burguer King", user: user)
-   }
+   let(:store) {create(:store, user: user)}
 
    describe "GET /index" do
-      it "renders a successful response with products data" do
-        Product.create! title: "macarrao", price: 1.00, description: "Macarrao saboroso da vovo", category: "massas", store_id: store.id
-        get "/stores/#{store.id}/products",
-        headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
-        json = JSON.parse(response.body)
-        expect(json[0]["title"]).to eq "macarrao"
-        expect(json[0]["price"].to_f).to eq 1.0
-        expect(json[0]["category"]).to eq "massas"
-      end
+     it "renders a successful response with products data" do
+       Product.create! title: "macarrao", price: 1.00, description: "Macarrao saboroso da vovo", category: "massas", store_id: store.id
+       get "/stores/#{store.id}/products",
+       headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
+       json = JSON.parse(response.body)
+       expect(json[0]["title"]).to eq "macarrao"
+       expect(json[0]["price"].to_f).to eq 1.0
+       expect(json[0]["category"]).to eq "massas"
+     end
+
+     it "returns a not found response if the product does not exist" do
+       get "/stores/#{store.id}/products/999",
+       headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
+       expect(response.status).to eq 404
+     end
    end
 
    describe "POST /products" do
@@ -71,6 +73,17 @@ RSpec.describe "/stores/:store_id/products", type: :request do
         expect(json["price"]).to eq "$2.00"
         expect(response.status).to eq 200
      end
+
+    it "returns a not found response if the product does not exist" do
+      put "/stores/#{store.id}/products/999",
+      headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"},
+      params: {
+        product: {
+          price: 2.00 
+        }
+      }
+      expect(response.status).to eq 404
+    end
    end
 
    describe "DELETE /products" do
