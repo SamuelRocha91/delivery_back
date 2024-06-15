fc1ed22a9664e0485b7fd16160f0e97f5433821e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-14 23:36:50 -0300

    feat: gera campos de ltitude e longitude na tabela de endereços

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_15_014743) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_023443) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_15_014743) do
     t.integer "addressable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable"
   end
 
