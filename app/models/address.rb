9e9622293fedc8a2f8887bb8067b88c7a1a9139a
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-14 23:32:21 -0300

    feat: cria métodos de cálculo de distancia na tabela adressable

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,22 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
+
+  geocoded_by :full_street_address
+
+  after_validation :geocode
+
+  def full_street_address
+    "#{street}, #{number}, #{city}, #{state}, #{postal_code}"
+  end
+
+  def distance_to(other_address)
+    return nil unless self.latitude && self.longitude && other_address.latitude && other_address.longitude
+
+    Geocoder::Calculations.distance_between(
+      [self.latitude, self.longitude],
+      [other_address.latitude, other_address.longitude]
+    )
+  end
+
 end
+
