00586c99832d4c63bfc0dd3c3e6e2d992254812a
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-30 23:40:10 -0300

    feat: atualiza configurações redis/sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
 end
