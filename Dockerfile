6ebc9585563012a7652ada3ffac23e988550233d
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-07-02 13:41:35 -0300

    feat: adapta dockerfile e action anacor pra gerar o heathmap sem a CA

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM ruby:3.2.2
 
-# Instalar dependências do sistema
 RUN apt-get update -qq && \
     apt-get install -y \
     build-essential \
@@ -10,27 +9,22 @@ RUN apt-get update -qq && \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
-# Instalar o bundler
 RUN gem install bundler
 
-# Definir diretório de trabalho
 WORKDIR /app
 
-# Copiar Gemfile e Gemfile.lock
 COPY Gemfile Gemfile.lock ./
 
-# Instalar gems
 RUN bundle install
 
-# Instalar pacotes R necessários
-RUN R -e "install.packages(c('FactoMineR', 'ggplot2', 'reshape2'), repos='https://cran.rstudio.com/')"
+RUN R -e "install.packages('ggplot2', repos='https://cran.rstudio.com/', dependencies=TRUE)"
+RUN R -e "install.packages('reshape2', repos='https://cran.rstudio.com/', dependencies=TRUE)"
 
-# Copiar o restante do código da aplicação
 COPY . .
 
 RUN mkdir -p tmp/cache/assets/sprockets && \
     chmod -R 755 tmp/cache/assets/sprockets
     
 EXPOSE 3000
-# Remove o arquivo de PID do servidor, se existir, e inicia o servidor Rails
+
 CMD ["sh", "-c", "rm -f /app/tmp/pids/server.pid && bundle exec rails server -b 0.0.0.0"]
--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -8,7 +8,6 @@ class AnalysisController < ApplicationController
 
     sales_data = Analysis.anacor(store_id)
     products = sales_data.map { |row| row['product'] }.uniq
-
     days_of_week = (0..6).to_a.map { |d| Date::DAYNAMES[d.to_i] }
 
     contingency_table = Array.new(products.length) { Array.new(days_of_week.length, 0) }
@@ -18,20 +17,12 @@ class AnalysisController < ApplicationController
       contingency_table[product_index][day_index] = row['sales_count'].to_i
     end
 
-    R.assign 'products', products
-    R.assign 'days_of_week', days_of_week
-    R.assign 'contingency_table', contingency_table.flatten
+    graph_process(products, days_of_week, contingency_table)
 
-    graph_process
     public_path = Rails.root.join('public', 'anacor_plot.png')
     FileUtils.mv('/tmp/anacor_plot.png', public_path)
 
-    coord_df = CSV.read('/tmp/coord_df.csv', headers: true)
-    coord_df = CSV.read('/tmp/coord_df.csv', headers: true)
-
-    result = coord_df.map { |row| { product: row['Product'], coordinates: { Dim1: row['Dim1'], Dim2: row['Dim2'] } } }
-
-    render json: { result: result, plot_image: '/anacor_plot.png' }
+    render json: { plot_image: '/anacor_plot.png' }
   end
 
 
@@ -78,9 +69,8 @@ class AnalysisController < ApplicationController
 
   private
 
-  def graph_process
+  def graph_process(products, days_of_week, contingency_table)
     R.eval <<-EOF
-      library(FactoMineR)
       library(ggplot2)
       library(reshape2)  # Adicionando o pacote reshape2
 
@@ -92,30 +82,23 @@ class AnalysisController < ApplicationController
         rownames(table_matrix) <- products
       }
 
-      # Executando análise de correspondência
-      anacor <- CA(table_matrix, graph = FALSE)
-      coord <- anacor$row$coord
-
-      # Preparando dados para o gráfico
-      coord_df <- data.frame(Product = products, Dim1 = coord[,1], Dim2 = coord[,2])
-
       # Gerando o heatmap
       p <- ggplot(data = melt(table_matrix), aes(x = Var2, y = Var1, fill = value)) +
         geom_tile() +
         scale_fill_gradient(low = "white", high = "blue") +
         labs(title = "Heatmap de Vendas por Produto e Dia da Semana",
-             x = "Dia da Semana",
-             y = "Produto",
-             fill = "Vendas") +
+            x = "Dia da Semana",
+            y = "Produto",
+            fill = "Vendas") +
         theme_minimal() +
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
       ggsave('/tmp/anacor_plot.png', plot = p, width = 8, height = 6)
 
-      # Salvando as coordenadas em um arquivo CSV
+      # Salvando as coordenadas em um arquivo CSV (se necessário)
+      coord_df <- data.frame(Product = products, Dim1 = 1:length(products), Dim2 = 1:ncol(table_matrix))
       write.csv(coord_df, '/tmp/coord_df.csv', row.names = FALSE)
     EOF
   end
-
 end
 
